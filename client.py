7ff1248e2b2f36289c656aed03a5084b417adeab
mario muuk <62722269+DrSuperMario@users.noreply.github.com>
2020-12-22 18:04:27 +0200

    updated

--- client.py
+++ client.py
@@ -1,12 +1,23 @@
+import sys
 import socket
+import selectors
+import types
+
+sel = selectors.DefaultSelector()
+
+_host = "127.0.0.1"
+_port = 65432
+_num_conns=1
 
 
-HOST = '127.0.0.1'
-PORT = 30548
+def start_conn(host, port, num_conns):
+    server_addr = (host, port) 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setblocking(False)
+    sock.connect_ex(server_addr)
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    data = b"Keepalive"
+    sel.register(sock, events, data=data)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'HELLOW WORLD')
-    data = s.recv(1024)
 
-print('recived ', repr(data))
+start_conn(_host, int(_port), int(_num_conns))
--- server.py
+++ server.py
@@ -1,20 +1,76 @@
+import sys
 import socket
+import selectors
+import types
+import time
+
+sel = selectors.DefaultSelector()
 
 
-HOST = '127.0.0.1'
-PORT = 30548
+_timelapsed_start = time.time()
+_host = "127.0.0.1"
+_port = 65432
+
+def accept(sock):
+    conn, addr = sock.accept()
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+def service(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+#open connection to TCP/IPv4
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((_host, _port))
+lsock.listen()
+print("listening on", (_host, _port))
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+try:
+    while True:
+
+        events = sel.select(timeout=None)
+        _timelapsed = time.time() - _timelapsed_start
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print("\n\t Connected: ", addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+        for key, mask in events:
 
+            if key.data is None:
+                _timelapsed_start = time.time()
+                _timelapsed = time.time() - _timelapsed_start
+                accept(key.fileobj)
+    
+            else:
+                while _timelapsed > 10.0:
+                    if(key.data is None):
+                        continue
+                    break
+            if(_timelapsed > 11):
+                sys.exit(1)    
 
+            print(_timelapsed)
+
+        
+                
+
+        #print(_timelapsed)
+
+except KeyboardInterrupt:
+    print("caught keyboard interrupt, exiting")
+finally:
+    sel.close()
